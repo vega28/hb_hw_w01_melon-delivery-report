d7fb5cd10f019f496e3d88f50ac0d7dcb659115f
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-02-25 05:35:28 +0000

    Added descriptive comments to produce_summary.py

--- produce_summary.py
+++ produce_summary.py
@@ -1,4 +1,5 @@
 def delivery_info(delivery_file):
+    """ take in one day's melon delivery log and print a summary to the screen """
     the_file = open(delivery_file)
     for line in the_file:
         line = line.rstrip()
@@ -11,7 +12,10 @@ def delivery_info(delivery_file):
         print(f'Delivered {count} {melon}s for total of ${amount}')
     the_file.close()
 
+
+# list the files to run through:
 delivery_files = ['um-deliveries-20140519.txt','um-deliveries-20140520.txt','um-deliveries-20140521.txt']
+# for each file in the list, print the day number and the delivery summary:
 for i, delivery_file in enumerate(delivery_files):
     print(f'Day {i+1}')
     delivery_info(delivery_file)
