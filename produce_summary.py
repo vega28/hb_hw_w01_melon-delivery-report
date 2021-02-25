26dc73602e776845312480de2f281ac3a8c5754e
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-02-25 05:32:49 +0000

    Restructured produce_summary.py to be less repetetive by using a function.

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,17 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def delivery_info(delivery_file):
+    the_file = open(delivery_file)
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+        melon = words[0]
+        count = words[1]    # original file had an error here - index was set to 0
+        amount = words[2]   # original file had an error here - index was set to 0
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+        print(f'Delivered {count} {melon}s for total of ${amount}')
+    the_file.close()
 
-
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+delivery_files = ['um-deliveries-20140519.txt','um-deliveries-20140520.txt','um-deliveries-20140521.txt']
+for i, delivery_file in enumerate(delivery_files):
+    print(f'Day {i+1}')
+    delivery_info(delivery_file)
